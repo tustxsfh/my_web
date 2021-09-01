f231bcb46c6b0bd3d8a956f3f85961f2d563ea53
tustxsfh <8573958+tustxsfh@user.noreply.gitee.com>
2021-09-01 12:40:03 +0800

    山西好干部在线学习

--- my_web/my_web/settings.py
+++ my_web/my_web/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '=q=y#4-i_7psh$uiff=u(hnpg2-icg@ris)@-+w9gs2u@sc%l+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -122,4 +122,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
+#
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+#
+# ]
+# STATICFILES_DIRS = ( '/home/tustxsfh/PycharmProjects/my_web/my_web/static', )
